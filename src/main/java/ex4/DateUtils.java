f643d867d6d49585952065bfb9da78f123ed8951
Lingatsu <115959500+Lingatsu@users.noreply.github.com>
2024-09-13 16:04:00 +0200

    Ex4 terminé

--- src/main/java/ex4/DateUtils.java
+++ src/main/java/ex4/DateUtils.java
@@ -5,15 +5,25 @@ import java.util.Date;
 
 public class DateUtils {
 
+    /**
+     * Formate une date selon le motif spécifié.
+     *
+     * @param pattern le motif de formatage
+     * @param date    la date à formater
+     * @return la date formatée en chaîne de caractères
+     */
     public static String format(String pattern, Date date) {
-
         SimpleDateFormat formateur = new SimpleDateFormat(pattern);
         return formateur.format(date);
     }
 
+    /**
+     * Formate une date en utilisant le motif de formatage par défaut.
+     *
+     * @param date la date à formater
+     * @return la date formatée en chaîne de caractères
+     */
     public static String formatDefaut(Date date) {
-
-        SimpleDateFormat formateur = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-        return formateur.format(date);
+        return format("dd/MM/yyyy HH:mm:ss", date);
     }
 }
